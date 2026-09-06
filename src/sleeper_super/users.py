ece18a39e931d4a68a1597f1b24886237c6a30dd
Alex Maliwat <atsm360@gmail.com>
2026-09-05 22:07:14 -0500

    feat (db): add roster initialization for mapping to users

--- src/sleeper_super/users.py
+++ src/sleeper_super/users.py
@@ -37,11 +37,47 @@ def init_users(settings: Settings) -> None:
                 (user["user_id"], user["display_name"])
             )
 
-def get_users(settings: Settings) -> pd.DataFrame:
+def init_rosters(settings: Settings) -> None:
+    with sqlite3.connect(settings.db_name) as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS rosters (
+                league_id INTEGER NOT NULL,
+                roster_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
 
+                PRIMARY KEY (league_id, roster_id),
+
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+        """)
+
+        leagues = pd.read_csv(settings.league_ids_path)
+
+        for league_id in leagues["id"]:
+            rosters = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}/rosters").json()
+            for roster in rosters:
+                conn.execute("""
+                    INSERT INTO rosters(league_id, roster_id, user_id) VALUES (?, ?, ?)
+                    ON CONFLICT (league_id, roster_id)
+                    DO UPDATE SET
+                        user_id = excluded.user_id
+                    """,
+                    (roster["league_id"], roster["roster_id"], roster["owner_id"])
+                ) 
+
+def get_users(settings: Settings) -> pd.DataFrame:
     with sqlite3.connect(settings.db_name) as conn:
         df = pd.read_sql_query(
             "SELECT * FROM users",
+            conn,
+        )
+    return df
+
+def get_user_rosters(settings: Settings, user_id: int) -> pd.DataFrame:
+    with sqlite3.connect(settings.db_name) as conn:
+        df = pd.read_sql_query(f"""
+            SELECT * FROM rosters
+            WHERE user_id = {user_id}""",
             conn,
         )
     return df
