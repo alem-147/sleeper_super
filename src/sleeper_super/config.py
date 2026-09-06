2eb00196accf82537694142412492ecead41cb63
Alex Maliwat <atsm360@gmail.com>
2026-09-06 10:12:24 -0500

    feat (db): dynamically set db name based on input csv

--- src/sleeper_super/config.py
+++ src/sleeper_super/config.py
@@ -1,11 +1,17 @@
 from pathlib import Path
+
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 class Settings(BaseSettings):
-    db_name: str
-    league_ids_path: str
+    league_ids_path: Path
+
+    @computed_field
+    @property
+    def db_name(self) -> str:
+        return f"{self.league_ids_path.stem}.db"
 
     model_config = SettingsConfigDict(env_file=PROJECT_ROOT / ".env", env_file_encoding='utf-8')
 
