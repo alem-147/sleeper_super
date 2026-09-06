ed72c507470e23863e4cdec611bad3ec4e2e5a07
Alex Maliwat <atsm360@gmail.com>
2026-09-06 10:49:10 -0500

    refactor (db): each function takes db connection instead of settings

--- src/sleeper_super/leagues.py
+++ src/sleeper_super/leagues.py
@@ -5,9 +5,8 @@ import pandas as pd
 
 from sleeper_super.config import Settings
 
-def init_leagues(settings: Settings) -> None:
-    with sqlite3.connect(settings.db_name) as conn:
-        conn.execute("""
+def init_leagues(conn: sqlite3.Connection, settings: Settings) -> None:
+    conn.execute("""
             CREATE TABLE IF NOT EXISTS leagues (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
@@ -16,30 +15,26 @@ def init_leagues(settings: Settings) -> None:
         """)
 
 
-        leagues = pd.read_csv(settings.league_ids_path)
+    leagues = pd.read_csv(settings.league_ids_path)
         
-        for league_id in leagues["id"]:
-            league = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}").json()
+    for league_id in leagues["id"]:
+        league = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}").json()
 
-            conn.execute("""
+        conn.execute("""
                 INSERT INTO leagues(id, name, season) VALUES (?, ?, ?)
                 ON CONFLICT (id)
                 DO UPDATE SET
                     name = excluded.name,
                     season = excluded.season
                 """,
-                (league["league_id"], league["name"], league["season"])
-            )
-
-def get_leagues(settings: Settings) -> pd.DataFrame:
-    with sqlite3.connect(settings.db_name) as conn:
-        df = pd.read_sql_query(
-            "SELECT * FROM leagues",
-            conn,
+            (league["league_id"], league["name"], league["season"])
         )
-    return df
+
+def get_leagues(conn: sqlite3.Connection) -> pd.DataFrame:
+    return pd.read_sql_query("SELECT * FROM leagues", conn)
 
 if __name__ == "__main__":
     settings = Settings()
-    init_leagues(settings)
-    print(get_leagues(settings).head())
+    with sqlite3.connect(settings.db_name) as conn:
+        init_leagues(conn, settings)
+        print(get_leagues(conn).head())
--- src/sleeper_super/users.py
+++ src/sleeper_super/users.py
@@ -11,43 +11,35 @@ class User:
     id: int
     display_name: str
 
-def init_users(settings: Settings) -> None:
-
-    with sqlite3.connect(settings.db_name) as conn:
-        conn.execute("""
+def init_users(conn: sqlite3.Connection, settings: Settings) -> None:
+    conn.execute("""
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY,
                 display_name TEXT NOT NULL
             )
         """)
 
-        leagues = pd.read_csv(settings.league_ids_path)
-        if "id" not in leagues.columns:
-            raise KeyError("'id' missing from leagues id csv")
-        league_id = leagues.loc[0]["id"]
+    leagues = pd.read_csv(settings.league_ids_path)
+    if "id" not in leagues.columns:
+        raise KeyError("'id' missing from leagues id csv")
+    league_id = leagues.loc[0]["id"]
 
-        users = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}/users").json()
+    users = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}/users").json()
 
-        for user in users:
-            conn.execute("""
+    for user in users:
+        conn.execute("""
                 INSERT INTO users (id, display_name) VALUES (?, ?)
                 ON CONFLICT (id)
                 DO UPDATE SET display_name = excluded.display_name
                 """,
-                (user["user_id"], user["display_name"])
-            )
-
-def get_users(settings: Settings) -> pd.DataFrame:
-    with sqlite3.connect(settings.db_name) as conn:
-        df = pd.read_sql_query(
-            "SELECT * FROM users",
-            conn,
+            (user["user_id"], user["display_name"])
         )
-    return df
 
-def init_rosters(settings: Settings) -> None:
-    with sqlite3.connect(settings.db_name) as conn:
-        conn.execute("""
+def get_users(conn: sqlite3.Connection) -> pd.DataFrame:
+    return pd.read_sql_query("SELECT * FROM users", conn)
+
+def init_rosters(conn: sqlite3.Connection, settings: Settings) -> None:
+    conn.execute("""
             CREATE TABLE IF NOT EXISTS rosters (
                 league_id INTEGER NOT NULL,
                 roster_id INTEGER NOT NULL,
@@ -59,21 +51,21 @@ def init_rosters(settings: Settings) -> None:
             )
         """)
 
-        leagues = pd.read_csv(settings.league_ids_path)
+    leagues = pd.read_csv(settings.league_ids_path)
 
-        for league_id in leagues["id"]:
-            rosters = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}/rosters").json()
-            for roster in rosters:
-                conn.execute("""
+    for league_id in leagues["id"]:
+        rosters = httpx.get(f"https://api.sleeper.app/v1/league/{league_id}/rosters").json()
+        for roster in rosters:
+            conn.execute("""
                     INSERT INTO rosters(league_id, roster_id, user_id) VALUES (?, ?, ?)
                     ON CONFLICT (league_id, roster_id)
                     DO UPDATE SET
                         user_id = excluded.user_id
                     """,
-                    (roster["league_id"], roster["roster_id"], roster["owner_id"])
-                )
+                (roster["league_id"], roster["roster_id"], roster["owner_id"])
+            )
 
-def get_rosters(settings: Settings, user_ids: list[int] | None = None) -> pd.DataFrame:
+def get_rosters(conn: sqlite3.Connection, user_ids: list[int] | None = None) -> pd.DataFrame:
     query = "SELECT * from rosters"
     params: tuple[int, ...] = ()
 
@@ -82,6 +74,4 @@ def get_rosters(settings: Settings, user_ids: list[int] | None = None) -> pd.Dat
         query += f" WHERE user_id IN ({placeholders})"
         params = tuple(user_ids)
 
-    with sqlite3.connect(settings.db_name) as conn:
-        return pd.read_sql_query(query, conn, params=params)
-
+    return pd.read_sql_query(query, conn, params=params)
